2b1851752f47191f3f68508a6231c50c9cc8912a
Stephen Yang <stepheny755@gmail.com>
2020-01-22 21:09:26 -0500

    Update Lab3.c

--- Other/Lab3.c
+++ Other/Lab3.c
@@ -1,43 +1,28 @@
 #include <stdio.h>
-/*
-int getIndexFromChar(char *charCode,char input){
-  if(*source==)
 
-
-}
-*/
 int main(void){
 
-  for(int i = 0;i < 8;i++){
-    printf("%d",i);
-  }
-  printf("\n");
-  for(int i = 0;i < 8;++i){
-    printf("%d",i);
-  }
-
-  char charCode[12] = {'k','b','r','o','e','g','u','v','y','w','l','s'};
-  char colorCode[12][6] = {"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White","Gold","Silver"};
-  char input = 'b';
-
-  //printf("%d",getIndexFromChar(&charCode,input))
+  char band1,band2,multiplier,tolerance;
 
   printf("Please enter the 1st band of resistor 1:\n");
-
+  scanf("%c",&band1);
   printf("Please enter the 2nd band of resistor 1:\n");
-
+  scanf("%c",&band2);
   printf("Please enter the multiplier band of resistor 1:\n");
-
+  scanf("%c",&multiplier);
   printf("Please enter the tolerance band of resistor 1:\n");
+  scanf("%c",&tolerance);
+
+
 
   printf("Please enter the 1st band of resistor 2:\n");
-
+  scanf("%c",&band1);
   printf("Please enter the 2nd band of resistor 2:\n");
-
+  scanf("%c",&band2);
   printf("Please enter the multiplier band of resistor 2:\n");
-
+  scanf("%c",&multiplier);
   printf("Please enter the tolerance band of resistor 2:\n");
-
+  scanf("%c",&tolerance);
 
   return 0;
 }
