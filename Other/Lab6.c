957c21afbd9d377ec919d2dd1d22a7923dd389c1
Stephen Yang <stepheny755@gmail.com>
2020-03-07 12:52:30 -0500

    Update Lab6.c
    added comments

--- Other/Lab6.c
+++ Other/Lab6.c
@@ -14,9 +14,11 @@ int getNumberofMines(int x,int y,int rows,int cols,char array[rows][cols]);
 
 int main(void){
 
+  //variable declarations
   int m,n;
   double p;
 
+  //get user input from console
   printf("Enter the number of rows: ");
   scanf("%d",&m);
   printf("Enter the number of columns: ");
@@ -24,21 +26,20 @@ int main(void){
   printf("Enter the probability p value: ");
   scanf("%lf",&p);
 
+  //create board of size 2 greater than required
+  //resolves array out of bounds errors
   int rows=m+2,cols=n+2;
 
   char board[rows][cols];
-  /*
-  int** board = (int**)malloc(sizeof(int*)*rows);
-  for(int i = 0;i < rows;i++){
-    board[i]=(int*)malloc(sizeof(int)*cols);
-  }
-  */
+
+  //initialize the board to all 0's
   for(int i = 0;i < rows;i++){
     for(int j = 0;j < cols;j++){
       board[i][j]=0;
     }
   }
 
+  //initialize the board with either bombs or spaces
   for(int i = 1;i < rows-1;i++){
     for(int j = 1;j < cols-1;j++){
       double probability=rand();
@@ -49,17 +50,21 @@ int main(void){
       }
     }
   }
+
+  //print board and lines
   printLine(cols);
   printArray(rows,cols,board);
 
+  //replace spaces with surrounding number of bombs
   for(int yval = 1;yval < rows-1;yval++){
     for(int xval = 1;xval < cols-1;xval++){
       if(board[yval][xval]=='.'){
-        //printf("%d,%d",yval,xval);
         board[yval][xval]=(char)getNumberofMines(xval,yval,rows,cols,board);
       }
     }
   }
+
+  //print board and lines
   printLine(cols);
   printArray(rows,cols,board);
   printLine(cols);
@@ -67,6 +72,7 @@ int main(void){
   return 0;
 }
 
+//prints one line of a certain length
 void printLine(int length){
   for(int i = 0;i<((length-2)*2)-1;i++){
     printf("-");
